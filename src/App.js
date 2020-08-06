c0137ce59ad34d73fb657e9137686ab27a8545b8
abhijeet <slashkirkmetallica@gmail.com>
2020-08-07 02:26:17 +0530

    Added Header component and navigation links

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homePage';
 import ShopPage from './pages/shopPage/shopPage';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
